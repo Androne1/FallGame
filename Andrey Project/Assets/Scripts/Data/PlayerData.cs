8a0e60913489d74808e88018a335d55122018443
Androne1 <andrewnaumov897@gmail.com>
2025-03-01 18:32:34 +0300

    I've done something

--- Andrey Project/Assets/Scripts/Data/PlayerData.cs
+++ Andrey Project/Assets/Scripts/Data/PlayerData.cs
@@ -1,22 +1,81 @@
 using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+[System.Serializable]
+public class SaveData
+{
+    public string currentSkins;
+    public List<string> unlockedSkons;
+    public int coins;
+    public int highScore;
+}
 
 public static class PlayerData
 {
-    private static int skinNum;
-    public static int SkinNum => skinNum;
+    private static string filePath = Path.Combine(Application.persistentDataPath, "playerdata.json");
+    private static string currentSkin;
+    private static List<string> unlockedSkins;
+    private static int coins;
+    private static int highScore;
+
+    public static string CurrentSkin => CurrentSkin;
+    public static List<string> UnlockedSkins => unlockedSkins;
+    public static int Coins => coins;
+    public static int HighScore => highScore;
 
     public static void Save()
     {
-        PlayerPrefs.SetInt("skinNum", skinNum);
+        SaveData data = new SaveData();
+        {
+            currentSkin = currentSkin;
+            unlockedSkins = unlockedSkins;
+            coins = coins;
+            highScore = highScore;
+        }
+
+        string json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(filePath, json);
     }
 
     public static void Load()
     {
-        skinNum = PlayerPrefs.GetInt("skinNum");
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            currentSkin = data.currentSkins;
+            unlockedSkins = data.unlockedSkons ?? new List<string>();
+            coins = data.coins;
+            highScore = data.highScore;
+        }
     }
 
-    public static void ChangeSkin(int num)
+    public static void ChangeSkin(string skinName)
     {
-        skinNum = num;
+        currentSkin = skinName;
+    }
+
+    public static void AddSkin(string skinName)
+    {
+        if(!unlockedSkins.Contains(skinName))
+        {
+            unlockedSkins.Add(skinName);
+        }
+    }
+
+    public static void AddCoins(int amount)
+    {
+        coins += amount;
+    }
+
+    public static void SetHighScore(int score)
+    {
+        if(score > highScore)
+        {
+            highScore = score;
+        }
     }
 }
--- Andrey Project/Assets/Scripts/UI/ShopButton.cs
+++ Andrey Project/Assets/Scripts/UI/ShopButton.cs
@@ -6,12 +6,13 @@ public class ShopButton : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI buttonText;
     [SerializeField] Button button;
-    [SerializeField] private TextMeshPro CostText;
+    [SerializeField] private TextMeshPro costText;
     [SerializeField] Transform visualPlace;
 
     public void Init(SingleSkinSO skin)
     {
         buttonText.text = skin.Name;
+        costText.text = skin.Price.ToString();
         Instantiate(skin.Visual, visualPlace);
     }
 }
